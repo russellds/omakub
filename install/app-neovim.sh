c2cd7a42c8003a9cb88c4d0dd832432e641f4e5a
David Heinemeier Hansson <david@hey.com>
2024-06-11 19:49:06 +0200

    We already have the right version in Ubuntu 24.04
    Closes #86

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -1,9 +1,4 @@
-cd /tmp
-wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
-tar -xf nvim.tar.gz
-sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
-rm -rf nvim-linux64 nvim.tar.gz
-cd -
+sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
